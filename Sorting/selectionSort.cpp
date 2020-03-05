f7a7771960d8c47f70bf6da9e813bf66c992f46f
CohleM <manish981194@gmail.com>
2020-03-05 09:15:17 +0545

    selectionSortImproved

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main(){
     int a[] = {2,7,4,1,5,3};
+    int lengthOfArray = sizeof(a)/sizeof(a[0]); // gives the length of our array a[].
 
-    for(int i=0; i < 6 - 1; i++ ){
+    for(int i=0; i < lengthOfArray - 1; i++ ){
         int min = i;
         for(int j = i+1; i<6; j++){
             if(a[j] < a[min]) min = j;
@@ -13,6 +14,6 @@ int main(){
         a[i] = a[min];
         a[min] = temp;
     }
-    for(auto b: a)
+    for(auto b: a) cout <<b <<" < "<<endl;
     return 0;
 }
